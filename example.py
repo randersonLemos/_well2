a2e8db3fd7321a4f4d2538ea8f7c307591b59d42
Randerson Lemos <randerson.lemos@gmail.com>
2019-10-24 21:54:11 -0300

    Removed additional interface to build wells.

--- example.py
+++ example.py
@@ -1,8 +1,103 @@
 from scripts.misc import Keywords as kw
+from scripts import frames
+from scripts import utils
+import pathlib
 
-def example1(): # producer
-    from scripts.utils import gen_prod_icv as gpi
+
+def gen_inje_icv(well, fluid, operate, monitor, completion, opening, on_time
+        , layerclump, icv_start, icv_control, output_folder):
+    w = frames.Frame_Inje_Dual(well, 'INJECTION')
+
+    w.get_incomp(fluid)
+
+    for ope in operate:
+        w.get_operate(*ope)
+
+    for mon in monitor:
+        w.get_monitor(*mon)
+
+    w.get_geometry('*K',0.108,0.370,1.0,0.0)
+    w.get_perf('*GEOA')
+
+    for com in utils.txt_to_lst(completion):
+        w.get_completion(com)
+
+    w.get_on_time(on_time)
+    w.get_open(opening)
+
+    for lay in layerclump:
+        w.get_layerclump(lay)
+
+    if icv_start:
+        w.get_icv_start(icv_start)
+        w.get_icv_control(icv_control)
+
+    w.build()
+    w.write(pathlib.Path(output_folder) / '{}.inc'.format(well))
+
+def gen_inje_wag(well, operate, monitor, completion, opening, on_time
+        , wag, layerclump, icv_start, icv_control, output_folder):
+    w = frames.Frame_Inje_Dual_Wag(well, 'INJECTION')
+
+    w.get_incomp('W','*WATER')
+    w.get_incomp('G','*GAS')
+
+    for ope in operate:
+        w.get_operate(*ope)
+
+    for mon in monitor:
+        w.get_monitor(*mon)
+
+    w.get_geometry('*K',0.108,0.370,1.0,0.0)
+    w.get_perf('*GEOA')
+
+    for com in utils.txt_to_lst(completion):
+        w.get_completion(com)
+
+    w.get_on_time(on_time)
+    w.get_open(*opening)
+
+    for lay in layerclump:
+        w.get_layerclump(lay)
+
+    w.get_wag(*wag)
+
+    w.build()
+    w.write(pathlib.Path(output_folder) / '{}.inc'.format(well))
+
+def gen_prod_icv(well, operate, monitor, completion, opening, on_time
+        , layerclump, icv_start, icv_control, output_folder):
+    w = frames.Frame_Prod_Dual(well, 'PRODUCTION')
+
+    for ope in operate:
+        w.get_operate(*ope)
+
+    for mon in monitor:
+        w.get_monitor(*mon)
+
+    w.get_geometry('*K',0.108,0.370,1.0,0.0)
+    w.get_perf('*GEOA')
+
+    for com in utils.txt_to_lst(completion):
+        w.get_completion(com)
+
+    w.get_on_time(on_time)
+    w.get_open(opening)
+
+    for lay in layerclump:
+        w.get_layerclump(lay)
+
+    if icv_start:
+        w.get_icv_start(icv_start)
+        w.get_icv_control(icv_control)
+
+    w.build()
+    w.write(pathlib.Path(output_folder) / '{}.inc'.format(well))
+
+
+if __name__ == '__main__':
     import infos.producers as ip
+    gpi = gen_prod_icv
     cont_repeat ='{} {}'.format(kw.cont(), kw.repeat())
 
     operate = [ (kw.max(), kw.stl(), 3000.0, cont_repeat)
@@ -21,12 +116,11 @@ def example1(): # producer
             , ip.layerclump_dic[well]
             , ip.icv_start_dic[well]
             , ip.icv_control_dic[well]
-            , './wells'
+            , './wells/producers'
             )
 
-def example2(): # injector of water
-    from scripts.utils import gen_inje_icv as gii
     import infos.injectors as ii
+    gii = gen_inje_icv
     cont_repeat = '{} {}'.format(kw.cont(),kw.repeat())
 
     operate = [ (kw.max(), kw.stw(), 5000.0, cont_repeat)
@@ -46,12 +140,11 @@ def example2(): # injector of water
             , ii.layerclump_dic[well]
             , []
             , []
-            , './wells'
+            , './wells/injectors'
             )
 
-def example3(): # injector with wag
-    from scripts.utils import gen_inje_wag as giw
     import infos.injectors_wag as iiw
+    giw = gen_inje_wag
     cont_repeat = '{} {}'.format(kw.cont(),kw.repeat())
 
     operate = [ ('G', kw.max(), kw.stg(), 3000000.0, cont_repeat)
@@ -73,10 +166,5 @@ def example3(): # injector with wag
             , iiw.layerclump_dic[well]
             , []
             , []
-            , './wells'
+            , './wells/injectors_wag'
             )
-
-if __name__ == '__main__':
-    example1()
-    example2()
-    example3()
